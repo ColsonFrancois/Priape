3bc3b714cd6b5240454eb4954e1f4095633a8d33
Francois <colsonfrancois@hormail.com>
2016-05-18 19:12:32 +0200

    +comment

---
+++ app/src/main/java/com/example/francois/priape/NewCommentActivity.java
@@ -0,0 +1,13 @@
+package com.example.francois.priape;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewCommentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_comment);
+    }
+}
